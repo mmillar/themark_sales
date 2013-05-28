8a460228674c798d67f803c0f4de3bb915bb1a24
Jeffrey Cheung <jcheung@verto.ca>
2013-05-28 17:21:38 -0400

    Removed footer icons and restored older version of .htaccess

--- code/wp-content/themes/marksales/footer.php
+++ code/wp-content/themes/marksales/footer.php
@@ -1,10 +1,10 @@
 	<footer>
 		<div id="footer">
 		  <div id="social_icons">
-		  	<div id="social_label">Share</div>
+		  	<!--<div id="social_label">Share</div>
 		  	<img src="<?php bloginfo('stylesheet_directory')?>/images/twitter_icon_2.gif">
 		  	<img src="<?php bloginfo('stylesheet_directory')?>/images/facebook_icon.gif">
-		  	<img src="<?php bloginfo('stylesheet_directory')?>/images/twitter_icon.gif">
+		  	<img src="<?php bloginfo('stylesheet_directory')?>/images/twitter_icon.gif">-->
 		  </div>
 		  <div id="disclaimer">
 		  	<img src="<?php bloginfo('stylesheet_directory')?>/images/site_logo_mini.gif"><br>
