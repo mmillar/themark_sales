94d1db0fb4a10316589ba94701c831563df84e1b
Jeffrey Cheung <jcheung@verto.ca>
2013-06-13 12:34:16 -0400

    Modified Unlisted page template for general use

--- code/wp-content/themes/marksales/unlisted.php
+++ code/wp-content/themes/marksales/unlisted.php
@@ -6,7 +6,9 @@
 
   <div id="content">
     <div id="unlisted">
-      <iframe src="http://player.vimeo.com/video/67066205" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; endif; ?>
     </div>
   </div>
 
