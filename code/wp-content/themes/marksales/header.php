c6153df52ab92f8c748abba2f39cbfbd04214f9d
Jeffrey Cheung <jcheung@verto.ca>
2013-06-20 10:01:53 -0400

    Restrict screen size for mobile

--- code/wp-content/themes/marksales/header.php
+++ code/wp-content/themes/marksales/header.php
@@ -11,7 +11,7 @@
 	<script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/style.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Roboto:700,300,900' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
 </head>
 
 <body>
@@ -23,11 +23,11 @@
 	  			<a href="<?php bloginfo('siteurl') ?>"><img src="<?php bloginfo('stylesheet_directory') ?>/images/site_logo.png"></a>
 	  		</div>
 	  		<div id="navigation" class="inactive">
-	  			<?php if($post->ID == 1) { ?><a class="active" id="header-current-page" href="<?php bloginfo('siteurl') ?>">WHAT WE DO</a><?php } ?>
-	  			<?php if($post->ID == 7) { ?><a class="active" id="header-current-page" href="?page_id=7">PAST EVENTS</a><?php } ?>
-	  			<?php if($post->ID == 9) { ?><a class="active" id="header-current-page" href="?page_id=9">UPCOMING EVENTS</a><?php } ?>
-	  			<?php if($post->ID == 11) { ?><a class="active" id="header-current-page" href="?page_id=11">WHO WE ARE</a><?php } ?>
-	  			<?php if($post->ID == 5) { ?><a class="active" id="header-current-page" href="?page_id=5">CONTACT</a><?php } ?>
+	  			<?php if($post->ID == 1) { ?><span class="yellow" id="header-current-page">WHAT WE DO</span><?php } ?>
+	  			<?php if($post->ID == 7) { ?><span class="yellow" id="header-current-page">PAST EVENTS</span><?php } ?>
+	  			<?php if($post->ID == 9) { ?><span class="yellow" id="header-current-page">UPCOMING EVENTS</span><?php } ?>
+	  			<?php if($post->ID == 11) { ?><span class="yellow" id="header-current-page">WHO WE ARE</span><?php } ?>
+	  			<?php if($post->ID == 5) { ?><span class="yellow" id="header-current-page">CONTACT</span><?php } ?>
 	  			<ul>
 	  				<li><a <?php if($post->ID == 1) echo 'class="active"' ?> href="<?php bloginfo('siteurl') ?>">WHAT WE DO</a></li>
 	  				<li><a <?php if($post->ID == 7) echo 'class="active"' ?> href="?page_id=7">PAST EVENTS</a></li>
